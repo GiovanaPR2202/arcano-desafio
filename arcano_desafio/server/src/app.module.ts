8f326bf085235dbdd9d7c5495892ef6d126a39c1
Giovana Pereira <41347753+GiovanaPR2202@users.noreply.github.com>
2023-05-03 06:57:15 -0300

    Delete arcano_desafio directory

--- arcano_desafio/server/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StoreapiModule } from './store-api/storeapi/storeapi.module';
-
-@Module({
-  imports: [StoreapiModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- arcano_desafio/server/src/store-api/interfaces/ClientInfo.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from './storeapi.interface';
-
-export interface ClientInfo {
-  id: number;
-  firstname: string;
-  lastname: string;
-  email: string;
-  products: Product[];
-}
